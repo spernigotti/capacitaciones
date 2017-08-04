831679c3b6d35df636c1a624e1d88c52daa74a56
santiago <s.a.pernigotti@gmail.com>
2017-08-04 01:16:03 -0300

    Agregada ES6 - tercera parte

---
+++ app.js
@@ -0,0 +1,53 @@
+const { lstatSync, readdirSync, watch, writeFileSync } = require('fs')
+const { join } = require('path')
+const inquirer = require('inquirer');
+const isDirectory = source => lstatSync(source).isDirectory();
+const isCourse = dir => !['node_modules', 'reveal', '.git', 'ignore'].includes(dir);
+const getDirectories = source =>
+  readdirSync(source).map(name => join(source, name)).filter(isDirectory).filter(isCourse);
+const pug = require('pug');
+const browserSync = require("browser-sync").create();
+
+const directories = getDirectories('.');
+
+async function start () {
+	const answers = await inquirer.prompt([{
+		type: 'list',
+		name: 'coursePath',
+		choices: directories,
+		message: 'Seleccione la capacitación:'
+	}]);
+
+	const { coursePath: slidesFolder } = answers;
+
+	const slidesFile = join(__dirname, slidesFolder, 'diapositivas.pug');
+	const slidesFileDist = join(slidesFolder, 'diapositivas.html');
+
+	compileSlides({ slidesFile, slidesFileDist, slidesFolder });
+	watch(slidesFile, () => compileSlides({ slidesFile, slidesFileDist, slidesFolder }));	
+
+	browserSync.init({
+		server: {
+			baseDir: join(__dirname, slidesFolder),
+			index: `diapositivas.html`
+		},
+		serveStatic: [{
+	        route: '/slides-src',
+	        dir: join(__dirname, 'slides-src/')
+	    }]
+	});
+
+	browserSync.reload('*.html');
+	browserSync.watch(slidesFileDist).on('change', browserSync.reload);
+}
+
+function compileSlides({ slidesFile, slidesFileDist, slidesFolder }) {
+	const options = {
+		pretty: true
+	};
+	const slidesHTML = pug.compileFile(slidesFile, options)();
+	
+	writeFileSync(slidesFileDist, slidesHTML, () => null);
+}
+
+start();
---
+++ es6 - tercera parte/nested-destructuring.js
@@ -0,0 +1,57 @@
+const order = {
+  "orderId": "v505203wofq-01",
+  "orderGroup": "v505203wofq",
+  "state": "canceled",
+  "value": 985,
+  "totals": [
+    {
+      "id": "Items",
+      "name": "Total de los itens",
+      "value": 485
+    },
+    {
+      "id": "Discounts",
+      "name": "Total de descuentos",
+      "value": 0
+    },
+    {
+      "id": "Shipping",
+      "name": "Costo total del envío",
+      "value": 500
+    },
+    {
+      "id": "Tax",
+      "name": "Costo total del cambio",
+      "value": 0
+    }
+  ],
+  "clientProfileData": {
+    "email": "santiago@fizzmod.com",
+    "firstName": "Santiago",
+    "lastName": "Fizzmod",
+  },
+  "shippingData": {
+    "address": {
+      "city": "Lima",
+      "state": "LIMA",
+      "country": "PER",
+      "street": "test",
+      "number": "123",
+      "neighborhood": "Surquillo",
+      "geoCoordinates": [
+        -77.01862335205078,
+        -12.11582351625169
+      ]
+    },
+    "logisticsInfo": [
+      {
+        "itemIndex": 0,
+        "selectedSla": "Entrega a domicilio",
+      }
+    ]
+  }
+};
+
+function showOrder() {
+
+}
